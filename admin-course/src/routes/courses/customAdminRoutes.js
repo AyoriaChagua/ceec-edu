a32cb435f9724df189ea231c0f77d747594318f8
ErikaVentura <105476284+erikaliane@users.noreply.github.com>
2023-11-03 23:41:03 -0500

    busqueda modulos por curso_id

--- admin-course/src/routes/courses/customAdminRoutes.js
+++ admin-course/src/routes/courses/customAdminRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router()
 const authenticateToken = require('../../middlewares/authenticationMiddleware')
 const customAdminController = require('../../controllers/courses/customAdminController')
 
-<<<<<<< HEAD
+
 const customModuleController = require('../../controllers/courses/customModuleController');
 
 router.get('/cursesmodules', authenticateToken, customAdminController.getCoursesModules);
-=======
+
 router.get('/coursesmodules', authenticateToken, customAdminController.getCoursesModules);
->>>>>>> 983aea78ee3507c0f62909ed8f1b053426724c9b
+
 
 router.get('/coursesuser/:id', authenticateToken, customAdminController.getCoursesByUser);
 
